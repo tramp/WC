96f9b35c338f76608bbe9e4ba31887bfe4a09bcb
Franz Haas <tramp@lavabit.com>
2010-04-26 20:24:30 +0200

    voting primitives added

--- code/internals/WC-internals-voting.lisp
+++ code/internals/WC-internals-voting.lisp
@@ -5,11 +5,21 @@
 (defun crypt_vote_step (vote pvd)
   (power 
    vote 
-   (key_pair-enc_key (private_vote_data-key_pair)) 
-   (rclass_totient-r_class (private_vote_data-r_class))))
+   (key_pair-enc_key (private_vote_data-key_pair pvd)) 
+   (rclass_totient-r_class (private_vote_data-r_class pvd))))
 
 (defun decrypt_vote_step (vote pvd)
   (power 
    vote 
-   (key_pair-dec_key (private_vote_data-key_pair)) 
-   (rclass_totient-r_class (private_vote_data-r_class))))
+   (key_pair-dec_key (private_vote_data-key_pair pvd)) 
+   (rclass_totient-r_class (private_vote_data-r_class pvd))))
+
+(defun create_vote (choice vtd)
+  (+ choice (random 
+	     (floor (rclass_totient-r_class 
+		     (vote_template_data-rclass_totient vtd)))
+	     (make-random-state t))))
+
+(defun extract_vote (pvd vtd)
+  (mod (private_vote_data-my_vote pvd)
+       (get_num_of_choices vtd)))
