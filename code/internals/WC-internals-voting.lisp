f55c6c7074459a62d1de9f44d58cd1771ecab3ac
Franz Haas <tramp@lavabit.com>
2010-04-29 17:00:20 +0200

    single crypt decrypt test added and passed

--- code/internals/WC-internals-voting.lisp
+++ code/internals/WC-internals-voting.lisp
@@ -6,13 +6,13 @@
   (power 
    vote 
    (key_pair-enc_key (private_vote_data-key_pair pvd)) 
-   (rclass_totient-r_class (private_vote_data-r_class pvd))))
+   (private_vote_data-r_class pvd)))
 
 (defun decrypt_vote_step (vote pvd)
   (power 
    vote 
    (key_pair-dec_key (private_vote_data-key_pair pvd)) 
-   (rclass_totient-r_class (private_vote_data-r_class pvd))))
+   (private_vote_data-r_class pvd)))
 
 (defun create_vote (choice vtd)
   (+ choice (random 
--- code/tests/voting-tests.lisp
+++ code/tests/voting-tests.lisp
@@ -14,3 +14,16 @@
 (5am:def-suite vote-suite)
 (5am:in-suite vote-suite)
 
+(5am:test crypt_decrypt_vote_once
+	  (5am:is (eq T
+		      (let ((pvd (let ((rct (generate_r_class_and_totient)))
+				   (make-private_vote_data
+				    :key_pair (generate_keys
+					       (rclass_totient-totient rct))
+				  :r_class (rclass_totient-r_class rct)
+				  :my_vote 0)))
+			      (vote 2))
+			    (= vote
+			      (decrypt_vote_step
+			       (crypt_vote_step vote pvd) pvd))))))
+(5am:run! 'vote-suite)
